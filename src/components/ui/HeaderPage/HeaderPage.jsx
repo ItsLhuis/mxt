4d1f7f585addb0519cef6816721a402f7c82cf08
Luis Rodrigues <itslhuis.dev@gmail.com>
2024-03-15 11:28:09 +0000

    Add button prop

--- src/components/ui/HeaderPage/HeaderPage.jsx
+++ src/components/ui/HeaderPage/HeaderPage.jsx
@@ -2,50 +2,68 @@ import PropTypes from "prop-types"
 
 import React from "react"
 
-import { Box, Typography, Breadcrumbs, Link } from "@mui/material"
+import { Box, Typography, Breadcrumbs, Link, Button } from "@mui/material"
 import { Home } from "@mui/icons-material"
 
-const BreadcrumbsSeparator = () => (
-  <Box
-    sx={{
-      width: 4,
-      height: 4,
-      borderRadius: "50%",
-      backgroundColor: "var(--outline)"
-    }}
-  />
-)
-
-const HeaderPage = ({ title, breadcrumbs }) => {
+const HeaderPage = ({ title, breadcrumbs, button }) => {
   return (
-    <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
-      <Typography variant="h4" component="h4">
-        {title}
-      </Typography>
-      <Breadcrumbs
-        separator={<BreadcrumbsSeparator />}
-        aria-label="breadcrumb"
-        sx={{ color: "var(--onSurface)", fontSize: 13 }}
-      >
-        <Home fontSize="small" />
-        {breadcrumbs.map((breadcrumb, index) => (
-          <Box key={index}>
-            {breadcrumb.link ? (
-              <Link underline="hover" href={breadcrumb.link} sx={{ color: "var(--onSurface)" }}>
-                {breadcrumb.name}
-              </Link>
-            ) : (
-              <Typography
-                variant="p"
-                component="p"
-                sx={{ color: "var(--outline)", fontWeight: 500 }}
-              >
-                {breadcrumb.name}
-              </Typography>
-            )}
-          </Box>
-        ))}
-      </Breadcrumbs>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        gap: 3
+      }}
+    >
+      <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
+        <Typography variant="h4" component="h4">
+          {title}
+        </Typography>
+        <Breadcrumbs
+          separator={
+            <Box
+              sx={{
+                width: 4,
+                height: 4,
+                borderRadius: "50%",
+                backgroundColor: "var(--outline)"
+              }}
+            />
+          }
+          aria-label="breadcrumb"
+          sx={{ color: "var(--onSurface)", fontSize: 13 }}
+        >
+          <Home fontSize="small" />
+          {breadcrumbs.map((breadcrumb, index) => (
+            <Box key={index}>
+              {breadcrumb.link ? (
+                <Link underline="hover" href={breadcrumb.link} sx={{ color: "var(--onSurface)" }}>
+                  {breadcrumb.name}
+                </Link>
+              ) : (
+                <Typography
+                  variant="p"
+                  component="p"
+                  sx={{ color: "var(--outline)", fontWeight: 500 }}
+                >
+                  {breadcrumb.name}
+                </Typography>
+              )}
+            </Box>
+          ))}
+        </Breadcrumbs>
+      </Box>
+      {button && (
+        <Button
+          variant="contained"
+          color="primary"
+          startIcon={button.leftIcon}
+          onClick={button.action}
+        >
+          {button.title}
+        </Button>
+      )}
     </Box>
   )
 }
@@ -57,7 +75,12 @@ HeaderPage.propTypes = {
       name: PropTypes.string.isRequired,
       link: PropTypes.string
     })
-  ).isRequired
+  ).isRequired,
+  button: PropTypes.shape({
+    leftIcon: PropTypes.element.isRequired,
+    title: PropTypes.string.isRequired,
+    action: PropTypes.func.isRequired
+  })
 }
 
 export default HeaderPage
