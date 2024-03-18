d9fe9b95d39f99988a3c7d7dbe70f35b93861813
Luis Rodrigues <itslhuis.dev@gmail.com>
2024-03-18 10:44:07 +0000

    Add the remaining components

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from "react"
+import React, { Suspense, useEffect } from "react"
 
 import { useLoader } from "@contexts/loaderContext"
 
-import { Box, Container } from "@mui/material"
+import { Box, Container, Grid } from "@mui/material"
 
-import { HeaderPage } from "@components/ui"
+import { PageLoader, HeaderPage } from "@components/ui"
+import { AnnualActivities, FinancialStatistics, Summary, ReparationsStates } from "./components"
 
 import { motion } from "framer-motion"
 
@@ -18,13 +19,27 @@ const Dashboard = () => {
   }, [])
 
   return (
-    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.2 }}>
-      <Box component="main" className="page-main">
-        <Container maxWidth={false}>
-          <HeaderPage title="Painel de Controlo" breadcrumbs={[{ name: "Painel de Controlo" }]} />
-        </Container>
-      </Box>
-    </motion.div>
+    <Suspense fallback={<PageLoader />}>
+      <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.2 }}>
+        <Box component="main" className="page-main">
+          <Container maxWidth={false}>
+            <HeaderPage title="Painel de Controlo" breadcrumbs={[{ name: "Painel de Controlo" }]} />
+            <Summary />
+            <Grid container spacing={3}>
+              <Grid item xs={12} md={12} lg={8}>
+                <AnnualActivities />
+              </Grid>
+              <Grid item xs={12} md={12} lg={4}>
+                <ReparationsStates />
+              </Grid>
+            </Grid>
+            <Box sx={{ paddingTop: 3 }}>
+              <FinancialStatistics />
+            </Box>
+          </Container>
+        </Box>
+      </motion.div>
+    </Suspense>
   )
 }
 
