565e786f77e06cf33bffbdd4fa4c70ce028d242e
Luis Rodrigues <itslhuis.dev@gmail.com>
2024-03-18 10:43:31 +0000

    Add imports for the remaining components

--- src/pages/Dashboard/components/index.js
+++ src/pages/Dashboard/components/index.js
@@ -1,5 +1,8 @@
 import { lazy } from "react"
 
+const AnnualActivities = lazy(() => import("./AnnualActivities/AnnualActivities"))
+const FinancialStatistics = lazy(() => import("./FinancialStatistics/FinancialStatistics"))
 const Summary = lazy(() => import("./Summary/Summary"))
+const ReparationsStates = lazy(() => import("./ReparationsStates/ReparationsStates"))
 
-export { Summary }
+export { AnnualActivities, FinancialStatistics, Summary, ReparationsStates }
