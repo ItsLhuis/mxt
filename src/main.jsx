a51d86aeef144d8fad72c23d2c5d923741a53114
Luis Rodrigues <itslhuis.dev@gmail.com>
2024-03-19 14:26:15 +0000

    Add date picker language and location setting

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
+import { LocalizationProvider } from "@mui/x-date-pickers"
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFnsV3"
+
 import { ThemeProvider } from "@contexts/themeContext"
 import { LoaderProvider } from "@contexts/loaderContext"
+import { pt } from "date-fns/locale/pt"
 
 import { BrowserRouter } from "react-router-dom"
 
@@ -13,12 +17,22 @@ import "./themes.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <LoaderProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </LoaderProvider>
-    </ThemeProvider>
+    <LocalizationProvider
+      dateAdapter={AdapterDateFns}
+      adapterLocale={pt}
+      localeText={{
+        cancelButtonLabel: "Cancelar",
+        okButtonLabel: "Selecionar",
+        datePickerToolbarTitle: "Selecione uma data"
+      }}
+    >
+      <ThemeProvider>
+        <LoaderProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </LoaderProvider>
+      </ThemeProvider>
+    </LocalizationProvider>
   </React.StrictMode>
 )
