545c76d36ac02d24bbd15c953158e24db1751eb4
Luis Rodrigues <itslhuis.dev@gmail.com>
2024-03-27 18:02:17 +0000

    Add forwardRef and other props

--- src/components/ui/Select/Select.jsx
+++ src/components/ui/Select/Select.jsx
@@ -1,69 +1,95 @@
 import PropTypes from "prop-types"
 
-import React from "react"
+import React, { forwardRef } from "react"
 
-import { FormControl, Select as MuiSelect, InputLabel, MenuItem, Typography } from "@mui/material"
+import {
+  FormControl,
+  FormHelperText,
+  Select as MuiSelect,
+  InputLabel,
+  MenuItem,
+  Typography,
+  Divider
+} from "@mui/material"
 import { KeyboardArrowDown } from "@mui/icons-material"
 
-const Select = ({ label, data, value, size, shrink, onChange }) => {
-  const handleChange = (event) => {
-    const value = event.target.value
-    if (onChange) {
-      onChange(value)
+const Select = forwardRef(
+  ({ label, data, value, size, shrink, error, helperText, onChange }, ref) => {
+    const handleChange = (event) => {
+      const value = event.target.value
+      if (onChange) {
+        onChange(value)
+      }
     }
-  }
 
-  return (
-    <FormControl fullWidth>
-      <InputLabel id="status-select-label" size={size} shrink={shrink}>
-        {label}
-      </InputLabel>
-      <MuiSelect
-        labelId="status-select-label"
-        id="status-select"
-        label={label + "o"}
-        value={value}
-        onChange={handleChange}
-        sx={{ fontSize: 13 }}
-        IconComponent={(props) => (
-          <KeyboardArrowDown {...props} sx={{ color: "var(--onSurface) !important" }} />
-        )}
-        size={size}
-      >
-        {data.map((item, index) => (
-          <MenuItem
-            key={item}
-            value={item}
-            sx={{
-              padding: "10px 16px",
-              margin: "0 8px",
-              marginBottom: index !== data.length - 1 && "4px",
-              borderRadius: "8px",
-              "&:hover": {
-                backgroundColor: "var(--secondaryContainer)"
-              }
-            }}
-          >
-            <Typography
-              variant="p"
-              component="p"
-              sx={{ overflow: "hidden", whiteSpace: "nowrap", textOverflow: "ellipsis" }}
+    return (
+      <FormControl fullWidth error={error}>
+        <InputLabel id="status-select-label" size={size} shrink={shrink} error={error}>
+          {label}
+        </InputLabel>
+        <MuiSelect
+          labelId="status-select-label"
+          id="status-select"
+          label={label + "o"}
+          value={value}
+          onChange={handleChange}
+          ref={ref}
+          sx={{ fontSize: 13 }}
+          IconComponent={(props) => (
+            <KeyboardArrowDown {...props} sx={{ color: "var(--onSurface) !important" }} />
+          )}
+          size={size}
+          error={error}
+        >
+          {data.map((item, index) => [
+            <MenuItem
+              key={item === "" ? "none" : item}
+              value={item === "" ? "" : item}
+              sx={{
+                padding: "10px 16px",
+                margin: "0 8px",
+                marginBottom: index !== data.length - 1 && "4px",
+                borderRadius: "8px",
+                "&:hover": {
+                  backgroundColor: "var(--secondaryContainer)"
+                }
+              }}
             >
-              {item}
-            </Typography>
-          </MenuItem>
-        ))}
-      </MuiSelect>
-    </FormControl>
-  )
-}
+              <Typography
+                variant="p"
+                component="p"
+                sx={{ overflow: "hidden", whiteSpace: "nowrap", textOverflow: "ellipsis" }}
+              >
+                {item === "" ? "Nenhum" : item}
+              </Typography>
+            </MenuItem>,
+            item === "" && (
+              <Divider
+                key={`divider-${index}`}
+                sx={{
+                  borderColor: "var(--elevation-level5)",
+                  borderWidth: 1
+                }}
+              />
+            )
+          ])}
+        </MuiSelect>
+        {helperText && (
+          <FormHelperText sx={{ color: error && "rgb(211, 47, 47)" }}>{helperText}</FormHelperText>
+        )}
+      </FormControl>
+    )
+  }
+)
 
 Select.propTypes = {
   label: PropTypes.string.isRequired,
   data: PropTypes.array.isRequired,
   value: PropTypes.string.isRequired,
   size: PropTypes.oneOf(["small", "medium", "large"]),
-  onChange: PropTypes.func.isRequired
+  onChange: PropTypes.func.isRequired,
+  error: PropTypes.bool,
+  helperText: PropTypes.string
 }
 
 export default Select
