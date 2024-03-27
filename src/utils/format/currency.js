f4e0465058480c6c7036f4da9d14733859735fa7
Luis Rodrigues <itslhuis.dev@gmail.com>
2024-03-27 18:05:03 +0000

    Update formatValueToEuro function

--- src/utils/format/currency.js
+++ src/utils/format/currency.js
@@ -1,5 +1,5 @@
-export const formatValueToEuro = (valueInCents) => {
-  const euros = (valueInCents / 100).toFixed(2).replace(".", ",")
+export const formatValueToEuro = (value) => {
+  const euros = parseFloat(value).toFixed(2).replace(".", ",")
   const eurosWithSpaces = euros.replace(/\B(?=(\d{3})+(?!\d))/g, " ")
   return eurosWithSpaces.endsWith(",00")
     ? eurosWithSpaces.slice(0, -3) + " €"
