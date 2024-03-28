0346eede475671fead859554d15e62ddf1dda3ed
Luis Rodrigues <itslhuis.dev@gmail.com>
2024-03-28 17:27:50 +0000

    Add error handling

--- src/components/ui/DatePicker/DatePicker.jsx
+++ src/components/ui/DatePicker/DatePicker.jsx
@@ -1,10 +1,31 @@
 import React from "react"
 
 import { DatePicker as MuiDatePicker } from "@mui/x-date-pickers"
+import { FormHelperText } from "@mui/material"
 import { KeyboardArrowDown } from "@mui/icons-material"
 
 const DatePicker = (props) => {
-  return <MuiDatePicker slots={{ switchViewIcon: KeyboardArrowDown }} {...props} />
+  return (
+    <>
+      <MuiDatePicker
+        slots={{
+          switchViewIcon: KeyboardArrowDown
+        }}
+        sx={{
+          "& .MuiOutlinedInput-notchedOutline": {
+            borderColor: props.error && "rgb(211, 47, 47) !important"
+          },
+          "& .MuiFormLabel-root": {
+            color: props.error && "rgb(211, 47, 47)!important"
+          }
+        }}
+        {...props}
+      />
+      <FormHelperText sx={{ color: props.error && "rgb(211, 47, 47)" }}>
+        {props.helperText}
+      </FormHelperText>
+    </>
+  )
 }
 
 export default DatePicker
