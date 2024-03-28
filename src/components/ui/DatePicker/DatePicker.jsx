6664fec46b5b5a6bc81ec97e8312899b2ba61d8c
Luis Rodrigues <itslhuis.dev@gmail.com>
2024-03-28 17:57:32 +0000

    Update error handling

--- src/components/ui/DatePicker/DatePicker.jsx
+++ src/components/ui/DatePicker/DatePicker.jsx
@@ -1,10 +1,13 @@
 import React from "react"
-
 import { DatePicker as MuiDatePicker } from "@mui/x-date-pickers"
 import { FormHelperText } from "@mui/material"
 import { KeyboardArrowDown } from "@mui/icons-material"
 
 const DatePicker = (props) => {
+  const { minDate, maxDate, value } = props
+  const isBeforeMinDate = minDate && value && value < minDate
+  const isAfterMaxDate = maxDate && value && value > maxDate
+
   return (
     <>
       <MuiDatePicker
@@ -13,16 +16,22 @@ const DatePicker = (props) => {
         }}
         sx={{
           "& .MuiOutlinedInput-notchedOutline": {
-            borderColor: props.error && "rgb(211, 47, 47) !important"
+            borderColor:
+              (props.error || isBeforeMinDate || isAfterMaxDate) && "rgb(211, 47, 47) !important"
           },
           "& .MuiFormLabel-root": {
-            color: props.error && "rgb(211, 47, 47)!important"
+            color:
+              (props.error || isBeforeMinDate || isAfterMaxDate) && "rgb(211, 47, 47) !important"
           }
         }}
         {...props}
       />
-      <FormHelperText sx={{ color: props.error && "rgb(211, 47, 47)" }}>
-        {props.helperText}
+      <FormHelperText
+        sx={{ color: (props.error || isBeforeMinDate || isAfterMaxDate) && "rgb(211, 47, 47)" }}
+      >
+        {(isBeforeMinDate && "A data selecionada está antes do limite mínimo") ||
+          (isAfterMaxDate && "A data selecionada está após o limite máximo") ||
+          props.helperText}
       </FormHelperText>
     </>
   )
