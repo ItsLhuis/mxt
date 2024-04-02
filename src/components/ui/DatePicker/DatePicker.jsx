948a85d16d8d0c564b0c4cb31d94876dfa07366f
Luis Rodrigues <itslhuis.dev@gmail.com>
2024-04-02 18:11:25 +0100

    Update component functionality

--- src/components/ui/DatePicker/DatePicker.jsx
+++ src/components/ui/DatePicker/DatePicker.jsx
@@ -1,7 +1,27 @@
 import React from "react"
+
 import { DatePicker as MuiDatePicker } from "@mui/x-date-pickers"
-import { FormHelperText } from "@mui/material"
+import { DialogActions, Button, FormHelperText } from "@mui/material"
 import { KeyboardArrowDown } from "@mui/icons-material"
+
+const CustomActionBar = (props) => {
+  const { onAccept, onCancel, ownerState, className } = props
+
+  const isMobile = ownerState.wrapperVariant !== "desktop"
+
+  if (isMobile) {
+    return (
+      <DialogActions className={className}>
+        <Button variant="contained" color="secondary" onClick={onCancel}>
+          Cancelar
+        </Button>
+        <Button variant="contained" color="primary" onClick={onAccept}>
+          Selecionar
+        </Button>
+      </DialogActions>
+    )
+  }
+}
 
 const DatePicker = (props) => {
   const { minDate, maxDate, value } = props
@@ -17,11 +37,14 @@ const DatePicker = (props) => {
   const isBeforeMinDate = strippedMinDate && strippedValue && strippedValue < strippedMinDate
   const isAfterMaxDate = strippedMaxDate && strippedValue && strippedValue > strippedMaxDate
 
+  const hasHelperText = isBeforeMinDate || isAfterMaxDate || props.helperText
+
   return (
     <>
       <MuiDatePicker
         slots={{
-          switchViewIcon: KeyboardArrowDown
+          switchViewIcon: KeyboardArrowDown,
+          actionBar: CustomActionBar
         }}
         sx={{
           "& .MuiOutlinedInput-notchedOutline": {
@@ -35,13 +58,15 @@ const DatePicker = (props) => {
         }}
         {...props}
       />
-      <FormHelperText
-        sx={{ color: (props.error || isBeforeMinDate || isAfterMaxDate) && "rgb(211, 47, 47)" }}
-      >
-        {(isBeforeMinDate && "A data selecionada está antes do limite mínimo") ||
-          (isAfterMaxDate && "A data selecionada está após o limite máximo") ||
-          props.helperText}
-      </FormHelperText>
+      {hasHelperText && (
+        <FormHelperText
+          sx={{ color: (props.error || isBeforeMinDate || isAfterMaxDate) && "rgb(211, 47, 47)" }}
+        >
+          {(isBeforeMinDate && "A data selecionada está antes do limite mínimo") ||
+            (isAfterMaxDate && "A data selecionada está após o limite máximo") ||
+            props.helperText}
+        </FormHelperText>
+      )}
     </>
   )
 }
