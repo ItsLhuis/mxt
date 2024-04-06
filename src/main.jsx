41d2efc086ac90e1ba84b3549fe09b265eb27060
Luis Rodrigues <itslhuis.dev@gmail.com>
2024-04-06 22:33:57 +0100

    Add new configs

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,12 @@ import { LoaderProvider } from "@contexts/loaderContext"
 
 import { BrowserRouter } from "react-router-dom"
 
+import { enableMapSet } from "immer"
+enableMapSet()
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+const queryClient = new QueryClient()
+
 import App from "./App.jsx"
 
 import "./index.css"
@@ -29,7 +35,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <ThemeProvider>
         <LoaderProvider>
           <BrowserRouter>
-            <App />
+            <QueryClientProvider client={queryClient}>
+              <App />
+            </QueryClientProvider>
           </BrowserRouter>
         </LoaderProvider>
       </ThemeProvider>
