14d3759a8398a66dd2ced614b828c168ac147a9c
Luis Rodrigues <itslhuis.dev@gmail.com>
2024-04-11 16:20:10 +0100

    Update some styles

--- src/components/ui/ListButton/ListButton.jsx
+++ src/components/ui/ListButton/ListButton.jsx
@@ -2,30 +2,55 @@ import PropTypes from "prop-types"
 
 import React from "react"
 
-import { List, ListItem, ListItemButton, ListItemText } from "@mui/material"
+import { List, ListItem, Divider, Button, Typography } from "@mui/material"
 
 const ListButton = ({ buttons, onClose }) => {
   return (
-    <List sx={{ minWidth: "90px" }}>
+    <List sx={{ display: "flex", flexDirection: "column", minWidth: "90px", gap: 0.5 }}>
       {buttons.map((button, index) => (
-        <ListItem key={index} disablePadding>
-          <ListItemButton
-            onClick={() => {
-              button.onClick()
-              onClose()
-            }}
-            sx={{
-              margin: "0 8px",
-              marginBottom: index !== buttons.length - 1 && "4px",
-              backgroundColor: button.selected && "rgba(88, 101, 242, 0.08)",
-              "&:hover": {
-                backgroundColor: button.selected && "rgba(88, 101, 242, 0.12) !important"
-              }
-            }}
-          >
-            <ListItemText primary={button.label} />
-          </ListItemButton>
-        </ListItem>
+        <React.Fragment key={index}>
+          {button.divider && (
+            <Divider
+              sx={{
+                marginBottom: 0.5,
+                marginTop: 0.5,
+                borderColor: "var(--elevation-level5)",
+                borderWidth: 1
+              }}
+            />
+          )}
+          <ListItem key={index} disablePadding>
+            <Button
+              color={button.color}
+              onClick={() => {
+                button.onClick()
+                onClose()
+              }}
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "flex-start",
+                margin: "0 8px !important",
+                padding: "8px 14px !important",
+                width: "100%",
+                color: button.color ?? "inherit",
+                marginBottom: index !== buttons.length - 1 && "4px",
+                backgroundColor: button.selected && "rgba(88, 101, 242, 0.08)",
+                minHeight: "unset !important",
+                "&:hover": {
+                  backgroundColor: button.selected
+                    ? "rgba(88, 101, 242, 0.12) !important"
+                    : button.color ?? "var(--secondaryContainer)"
+                }
+              }}
+            >
+              <Typography variant="p" component="p" sx={{ fontWeight: 400 }}>
+                {button.label}
+              </Typography>
+            </Button>
+          </ListItem>
+        </React.Fragment>
       ))}
     </List>
   )
