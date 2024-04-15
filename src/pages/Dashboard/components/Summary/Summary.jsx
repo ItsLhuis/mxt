109c37196a05d9d84df5405b6c6ebab8f8a4a7ce
Luis Rodrigues <itslhuis.dev@gmail.com>
2024-04-15 18:07:41 +0100

    Add loading state

--- src/pages/Dashboard/components/Summary/Summary.jsx
+++ src/pages/Dashboard/components/Summary/Summary.jsx
@@ -1,119 +1,100 @@
 import React from "react"
 
-import { Grid, Paper, Box, Typography, Chip, Stack } from "@mui/material"
+import { useQuery } from "@tanstack/react-query"
+
+import { Grid } from "@mui/material"
 import { Person, AppsOutlined, Construction } from "@mui/icons-material"
 
-import { Caption } from "@components/ui"
-import { BasicLineChart } from "@components/ui/Charts"
+import { SummaryCard } from "./components"
 
 import { formatMonth } from "@utils/format/date"
 
-const Summary = () => {
-  const getLatestEightDates = () => {
-    const today = new Date()
-    const dates = [today]
+const getLatestEightDates = () => {
+  const today = new Date()
+  const dates = [today]
 
-    for (let i = 1; i < 8; i++) {
-      const date = new Date(today.getFullYear(), today.getMonth() - i, today.getDate())
-      dates.push(date)
-    }
-
-    dates.sort((a, b) => a - b)
-
-    const formattedDates = dates.map((date) => formatMonth(date))
-
-    return formattedDates
+  for (let i = 1; i < 8; i++) {
+    const date = new Date(today.getFullYear(), today.getMonth() - i, today.getDate())
+    dates.push(date)
   }
 
-  const data = [
+  dates.sort((a, b) => a - b)
+
+  const formattedDates = dates.map((date) => formatMonth(date))
+
+  return formattedDates
+}
+
+const fetchMetricData = async (type) => {
+  await new Promise((resolve) => setTimeout(resolve, 3000))
+
+  const data = {
+    clients: { total: 503, change: "+ 27,11%", color: "success" },
+    equipments: { total: 735, change: "- 2,43%", color: "error" },
+    repairs: { total: 3921, change: "+ 4,78%", color: "success" }
+  }
+
+  return data[type]
+}
+
+const fetchChartData = async (type) => {
+  await new Promise((resolve) => setTimeout(resolve, 4000))
+
+  const latestDates = getLatestEightDates()
+
+  const data = {
+    clients: { xData: latestDates, yData: [1, 74, 18, 68, 65, 82, 92, 68] },
+    equipments: { xData: latestDates, yData: [15, 59, 46, 50, 95, 40, 0, 31] },
+    repairs: { xData: latestDates, yData: [2, 72, 63, 14, 47, 87, 30, 74] }
+  }
+
+  return data[type]
+}
+
+const Summary = () => {
+  const types = [
+    { type: "clients", icon: <Person />, title: "Clientes", colorLine: "rgb(248, 112, 96)" },
     {
-      icon: <Person />,
-      title: "Clientes",
-      value: 503,
-      change: "+ 27,11%",
-      color: "success",
-      colorLine: "rgb(248, 112, 96)",
-      xData: getLatestEightDates(),
-      yData: Array.from({ length: 8 }, () => Math.floor(Math.random() * 100))
-    },
-    {
+      type: "equipments",
       icon: <AppsOutlined />,
       title: "Equipamentos",
-      value: 735,
-      change: "- 2,43%",
-      color: "error",
-      colorLine: "rgb(165, 170, 82)",
-      xData: getLatestEightDates(),
-      yData: Array.from({ length: 8 }, () => Math.floor(Math.random() * 300))
+      colorLine: "rgb(165, 170, 82)"
     },
     {
+      type: "repairs",
       icon: <Construction />,
       title: "Reparações",
-      value: 3921,
-      change: "+ 4,78%",
-      color: "success",
-      colorLine: "rgb(124, 152, 179)",
-      xData: getLatestEightDates(),
-      yData: Array.from({ length: 8 }, () => Math.floor(Math.random() * 1000))
+      colorLine: "rgb(124, 152, 179)"
     }
   ]
 
-  const mdSize = data.length % 2 === 0 ? 6 : 12
+  const mdSize = types.length % 2 === 0 ? 6 : 12
 
   return (
     <Grid container spacing={3}>
-      {data.map((item, index) => (
-        <Grid key={index} item xs={12} md={index === data.length - 1 ? mdSize : 6} lg={4}>
-          <Paper elevation={1}>
-            <Box sx={{ display: "flex", alignItems: "center", width: "100%" }}>
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "flex-start",
-                  gap: 1.3,
-                  padding: 3
-                }}
-              >
-                <Box
-                  sx={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    padding: 1.5,
-                    borderRadius: 2,
-                    backgroundColor: "var(--elevation-level3)"
-                  }}
-                >
-                  {item.icon}
-                </Box>
-                <Typography variant="p" component="p" sx={{ fontWeight: 600 }}>
-                  {item.title}
-                </Typography>
-                <Typography variant="h3" component="h3">
-                  {item.value}
-                </Typography>
-                <Stack sx={{ flexDirection: "row", alignItems: "center", gap: 1 }}>
-                  <Chip label={item.change} color={item.color} sx={{ marginTop: "auto" }} />
-                  <Caption title="Em comparação com o mês anterior" />
-                </Stack>
-              </Box>
-              <Box sx={{ width: "100%", height: 180, marginRight: 3 }}>
-                <BasicLineChart
-                  colorLine={item.colorLine}
-                  xData={item.xData}
-                  yData={[
-                    {
-                      name: item.title,
-                      data: item.yData
-                    }
-                  ]}
-                />
-              </Box>
-            </Box>
-          </Paper>
-        </Grid>
-      ))}
+      {types.map(({ type, icon, title, colorLine }, index) => {
+        const metricQuery = useQuery({
+          queryKey: [`dashboard${type.charAt(0).toUpperCase() + type.slice(1)}MetricData`],
+          queryFn: () => fetchMetricData(type)
+        })
+
+        const chartQuery = useQuery({
+          queryKey: [`dashboard${type.charAt(0).toUpperCase() + type.slice(1)}ChartData`],
+          queryFn: () => fetchChartData(type)
+        })
+
+        return (
+          <SummaryCard
+            key={type}
+            icon={icon}
+            title={title}
+            metricQuery={metricQuery}
+            chartQuery={chartQuery}
+            colorLine={colorLine}
+            mdSize={index === types.length - 1 ? mdSize : 6}
+          />
+        )
+      })}
     </Grid>
   )
 }
---
+++ src/pages/Dashboard/components/Summary/components/SummaryCard/SummaryCard.jsx
@@ -0,0 +1,138 @@
+import PropTypes from "prop-types"
+
+import React from "react"
+
+import {
+  Grid,
+  Paper,
+  Box,
+  Typography,
+  Chip,
+  Stack,
+  CircularProgress,
+  Skeleton
+} from "@mui/material"
+
+import { Caption } from "@components/ui"
+import { BasicLineChart } from "@components/ui/Charts"
+
+import { motion } from "framer-motion"
+
+const MetricSkeleton = () => {
+  return (
+    <Stack sx={{ gap: 1.3 }}>
+      <Typography variant="h3" component="h3">
+        <Skeleton width={60} />
+      </Typography>
+      <Stack sx={{ flexDirection: "row", alignItems: "center", gap: 1 }}>
+        <Skeleton variant="rounded" width={80} height={32} sx={{ borderRadius: 2 }} />
+        <Skeleton variant="circular" width={13} height={13} />
+      </Stack>
+    </Stack>
+  )
+}
+
+const SummaryCard = ({ icon, title, metricQuery, chartQuery, colorLine, mdSize }) => {
+  return (
+    <Grid item xs={12} md={mdSize} lg={4}>
+      <Paper elevation={1}>
+        <Box sx={{ display: "flex", alignItems: "center", width: "100%" }}>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "flex-start",
+              gap: 1.3,
+              padding: 3
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                padding: 1.5,
+                borderRadius: 2,
+                backgroundColor: "var(--elevation-level3)"
+              }}
+            >
+              {icon}
+            </Box>
+            <Typography variant="p" component="p" sx={{ fontWeight: 600 }}>
+              {title}
+            </Typography>
+            {metricQuery.isLoading ? (
+              <MetricSkeleton />
+            ) : (
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.2 }}
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "flex-start",
+                  gap: "10.4px"
+                }}
+              >
+                <Typography variant="h3" component="h3">
+                  {metricQuery?.data.total}
+                </Typography>
+                <Stack sx={{ flexDirection: "row", alignItems: "center", gap: 1 }}>
+                  <Chip
+                    label={metricQuery?.data.change}
+                    color={metricQuery?.data.color}
+                    sx={{ marginTop: "auto" }}
+                  />
+                  <Caption title="Em comparação com o mês anterior" />
+                </Stack>
+              </motion.div>
+            )}
+          </Box>
+          <Box
+            sx={{
+              width: "100%",
+              marginRight: 3,
+              display: chartQuery.isLoading && "flex",
+              justifyContent: "center"
+            }}
+          >
+            {chartQuery.isLoading ? (
+              <CircularProgress />
+            ) : (
+              <BasicLineChart
+                colorLine={colorLine}
+                xData={chartQuery?.data.xData}
+                yData={[{ name: title, data: chartQuery?.data.yData }]}
+              />
+            )}
+          </Box>
+        </Box>
+      </Paper>
+    </Grid>
+  )
+}
+
+SummaryCard.propTypes = {
+  icon: PropTypes.node.isRequired,
+  title: PropTypes.string.isRequired,
+  metricQuery: PropTypes.shape({
+    isLoading: PropTypes.bool.isRequired,
+    data: PropTypes.shape({
+      total: PropTypes.number.isRequired,
+      change: PropTypes.string.isRequired,
+      color: PropTypes.string.isRequired
+    })
+  }).isRequired,
+  chartQuery: PropTypes.shape({
+    isLoading: PropTypes.bool.isRequired,
+    data: PropTypes.shape({
+      xData: PropTypes.arrayOf(PropTypes.string.isRequired),
+      yData: PropTypes.arrayOf(PropTypes.number.isRequired)
+    })
+  }).isRequired,
+  colorLine: PropTypes.string.isRequired,
+  mdSize: PropTypes.number.isRequired
+}
+
+export default SummaryCard
