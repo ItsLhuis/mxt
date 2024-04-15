a1953f7836af1004ddd76b101ebdfabe28f05801
Luis Rodrigues <itslhuis.dev@gmail.com>
2024-04-15 17:58:04 +0100

    Update exports

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -1,16 +1,32 @@
 export { default as ButtonDropDownSelect } from "./ButtonDropDownSelect/ButtonDropDownSelect"
+
 export { default as Caption } from "./Caption/Caption"
+
 export { default as CommandDialog } from "./CommandDialog/CommandDialog"
+
 export { default as DatePicker } from "./DatePicker/DatePicker"
+
 export { default as HeaderPage } from "./HeaderPage/HeaderPage"
+
 export { default as ImagePicker } from "./ImagePicker/ImagePicker"
+
 export { default as ListButton } from "./ListButton/ListButton"
+
 export { default as Load } from "./Load/Load"
+
 export { default as Loader } from "./Loader/Loader"
+
 export { default as Modal } from "./Modal/Modal"
+
 export { default as MultipleSelectCheckmarks } from "./MultipleSelectCheckmarks/MultipleSelectCheckmarks"
+
 export { default as NoData } from "./NoData/NoData"
+
 export { default as PageLoader } from "./PageLoader/PageLoader"
+
 export { default as PageProgress } from "./PageProgress/PageProgress"
+
 export { default as Select } from "./Select/Select"
+
 export { default as Table } from "./Table/Table"
+export { default as TableSkeleton } from "./Table/TableSkeleton"
