70e669ae9f5c88833b6c1c00de1fb445721ebb76
Luis Rodrigues <itslhuis.dev@gmail.com>
2024-04-18 17:33:23 +0100

    Update all application functionality

--- classes/app/error.js
+++ classes/app/error.js
@@ -1,12 +1,11 @@
 class AppError extends Error {
-    constructor(statusCode, errorCode, message, isOperational = false) {
-      super(message)
-      this.statusCode = statusCode
-      this.errorCode = errorCode
-      this.errorMessage = message
-      this.isOperational = isOperational
-    }
+  constructor(statusCode, errorCode, message, isOperational = false) {
+    super(message)
+    this.statusCode = statusCode
+    this.errorCode = errorCode
+    this.errorMessage = message
+    this.isOperational = isOperational
   }
-  
-  module.exports = AppError
-  
+}
+
+module.exports = AppError
