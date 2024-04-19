48ea12c6fe4950881fe76341b996d49ed17175d5
Luis Rodrigues <itslhuis.dev@gmail.com>
2024-04-19 18:38:34 +0100

    Update controller functionality

--- controllers/user.js
+++ controllers/user.js
@@ -1,10 +1,21 @@
+const bcrypt = require("bcrypt")
+
 const AppError = require("@classes/app/error")
 const { tryCatch } = require("@utils/tryCatch")
 
-const { USERNAME_ALREADY_EXISTS, EMAIL_ALREADY_EXISTS } = require("@constants/errors/user")
+const { SALT_ROUNDS } = require("@constants/bcrypt")
+
+const {
+  USERNAME_ALREADY_EXISTS,
+  EMAIL_ALREADY_EXISTS,
+  USER_NOT_FOUND,
+  PASSWORD_MISMATCH
+} = require("@constants/errors/user")
 
 const User = require("@models/user")
-const { createUserSchema } = require("@schemas/user")
+const { createUserSchema, updateUserSchema, updateUserPasswordSchema } = require("@schemas/user")
+
+const Employee = require("@models/employee")
 
 const userController = {
   findAll: tryCatch(async (req, res) => {
@@ -18,16 +29,75 @@ const userController = {
 
     const existingUsername = await User.findByUsername(username)
     if (existingUsername.length > 0) {
-      throw new AppError(400, USERNAME_ALREADY_EXISTS, "Nome de utilizador já existente", true)
+      throw new AppError(400, USERNAME_ALREADY_EXISTS, "The username already exists", true)
     }
 
     const existingEmail = await User.findByEmail(email)
     if (existingEmail.length > 0) {
-      throw new AppError(400, EMAIL_ALREADY_EXISTS, "E-mail já existente", true)
+      throw new AppError(400, EMAIL_ALREADY_EXISTS, "The e-mail already exists", true)
     }
 
-    const newUser = await User.create(username, password, email, role, isActive)
-    res.status(201).json(newUser)
+    const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS)
+
+    const user = await User.create(username, hashedPassword, email, role, isActive)
+    await Employee.create(user.insertId)
+    res.status(201).json({ message: "User created successfully" })
+  }),
+  update: tryCatch(async (req, res) => {
+    const { id } = req.params
+    const { username, email, role, isActive } = req.body
+
+    const existingUser = await User.findById(id)
+    if (!existingUser.length) {
+      throw new AppError(404, USER_NOT_FOUND, "User not found", true)
+    }
+
+    updateUserSchema.parse(req.body)
+
+    const existingUsername = await User.findByUsername(username)
+    if (existingUsername.length > 0 && existingUsername[0].id !== parseInt(id)) {
+      throw new AppError(400, USERNAME_ALREADY_EXISTS, "The username already exists", true)
+    }
+
+    const existingEmail = await User.findByEmail(email)
+    if (existingEmail.length > 0 && existingEmail[0].id !== parseInt(id)) {
+      throw new AppError(400, EMAIL_ALREADY_EXISTS, "The e-mail already exists", true)
+    }
+
+    await User.update(id, username, email, role, isActive)
+    res.status(204).json({ message: "User updated successfully" })
+  }),
+  updatePassword: tryCatch(async (req, res) => {
+    const { id } = req.params
+    const { password, newPassword } = req.body
+
+    const existingUser = await User.findById(id)
+    if (!existingUser.length) {
+      throw new AppError(404, USER_NOT_FOUND, "User not found", true)
+    }
+
+    updateUserPasswordSchema.parse(req.body)
+
+    const isPasswordMatch = await bcrypt.compare(password, existingUser[0].password)
+    if (!isPasswordMatch) {
+      throw new AppError(400, PASSWORD_MISMATCH, "Current password does not match", true)
+    }
+
+    const hashedNewPassword = await bcrypt.hash(newPassword, SALT_ROUNDS)
+
+    User.updatePassword(id, hashedNewPassword)
+    res.status(204).json({ message: "Password updated successfully" })
+  }),
+  delete: tryCatch(async (req, res) => {
+    const { id } = req.params
+
+    const existingUser = await User.findById(id)
+    if (!existingUser.length) {
+      throw new AppError(404, USER_NOT_FOUND, "User not found", true)
+    }
+
+    await User.delete(id)
+    res.status(204).json({ message: "User removed successfully" })
   })
 }
 
