a6b2e4c63c871ab7fa4579528055775bdb5d92c5
Luis Rodrigues <itslhuis.dev@gmail.com>
2024-04-19 18:39:19 +0100

    Update routes

--- routes/user.js
+++ routes/user.js
@@ -5,5 +5,8 @@ const userController = require("@controllers/user")
 
 router.get("/", userController.findAll)
 router.post("/", userController.create)
+router.put("/:id", userController.update)
+router.put("/:id/password", userController.updatePassword)
+router.delete("/:id", userController.delete)
 
 module.exports = router
