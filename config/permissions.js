981f29a7e272a0c13dc678a85b145a043a9da978
Luis Rodrigues <itslhuis.dev@gmail.com>
2024-05-06 18:21:01 +0100

    Update permissions

--- config/permissions.js
+++ config/permissions.js
@@ -2,6 +2,11 @@ const roles = require("@constants/roles")
 const permissions = require("@constants/permissions")
 
 module.exports = {
+  user: {
+    [roles.BOSS]: [permissions.ALL],
+    [roles.ADMIN]: [permissions.ALL],
+    [roles.EMPLOYEE]: [permissions.UPDATE]
+  },
   client: {
     [roles.BOSS]: [permissions.ALL],
     [roles.ADMIN]: [permissions.ALL],
@@ -18,8 +23,7 @@ module.exports = {
     },
     interactionsHistory: {
       [roles.BOSS]: [permissions.READ],
-      [roles.ADMIN]: [permissions.READ],
-      [roles.EMPLOYEE]: [permissions.READ]
+      [roles.ADMIN]: [permissions.READ]
     }
   }
 }
