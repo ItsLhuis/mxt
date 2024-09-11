ed1f152afeeaaf7c114f69d93de5f9653c2e519e
Luis Rodrigues <itslhuis.dev@gmail.com>
2024-09-11 14:36:31 +0100

    Update create attachment text

--- constants/interactions/repair.js
+++ constants/interactions/repair.js
@@ -1,6 +1,6 @@
 module.exports = {
   REPAIR_CREATED: "Reparação Criada",
   REPAIR_UPDATED: "Reparação Atualizada",
-  REPAIR_ATTACHMENT_CREATED: "Anexo Adicionado",
+  REPAIR_ATTACHMENT_CREATED: "Anexo(s) Adicionado(s)",
   REPAIR_ATTACHMENT_DELETED: "Anexo Eliminado"
 }
