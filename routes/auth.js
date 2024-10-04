2e65a22bb3a48deadc59983f9ff8bcea823bddd3
Luis Rodrigues <itslhuis.dev@gmail.com>
2024-10-04 15:02:06 +0100

    Update resetPassword routes to reset-password

--- routes/auth.js
+++ routes/auth.js
@@ -7,8 +7,8 @@ router.post("/login", authController.login)
 router.post("/refreshToken", authController.refreshToken)
 router.delete("/logout", authController.logout)
 
-router.post("/resetPassword/request", authController.resetPassword.request)
-router.post("/resetPassword/verify/:token", authController.resetPassword.verify)
-router.post("/resetPassword/confirm/:token", authController.resetPassword.confirm)
+router.post("/reset-password/request", authController.resetPassword.request)
+router.post("/reset-password/verify/:token", authController.resetPassword.verify)
+router.post("/reset-password/confirm/:token", authController.resetPassword.confirm)
 
 module.exports = router
